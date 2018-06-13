f4ec0af13c0ef173bca92e59daea267018661f67
Maximilian Witte <maximilianwitte@outlook.de>
2018-06-13 16:39:29 +0200

    Updated Css, Readme, Image Structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Install and import babel-polyfill to get react ready to fetch as google.
+import 'babel-polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
